48d845a0d8b3d330ceb8486a46f2a7ff64c7c216
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-04-19 20:00:17 -0400

    Add skeleton lib/ directory.
    The lib/ directory currently contains three bare-bones libraries.
    These libraries will be built up in upcoming commits.

---
+++ lib/disk.asm
@@ -0,0 +1,8 @@
+#ifndef MIT6115_JPR_DISK_H_
+#define MIT6115_JPR_DISK_H_
+;;; BEGIN DISK LIBRARY
+disk_init:
+	;; TODO(jasonpr): Implement
+	ret
+;;; END DISK LIBRARY
+#endif
---
+++ lib/serial.asm
@@ -0,0 +1,8 @@
+#ifndef MIT6115_JPR_SERIAL_H_
+#define MIT6115_JPR_SERIAL_H_
+;;; BEGIN SERIAL LIBRARY
+serial_init:
+	;; TODO(jasonpr): Implement.
+	ret
+;;; END SERIAL LIBRARY
+#endif
