1a0000c1d33a65d71706e0e4e0b73c640889f14b
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-04-29 15:55:38 -0400

    Add additional SD commands to diagnostic program.
    Specifically:
    * Allow sending of arbitrary commands to SD card.
    * Show response of CMD8 in 'C' (SD init) commmand.

--- diagnostic.asm
+++ diagnostic.asm
@@ -33,6 +33,10 @@ diag_loop:
 	jz diag_sd_test
 	xrl a, #'C'
 
+	xrl a, #'M'
+	jz diag_sd_msg
+	xrl a, #'M'
+
 diag_cleanup:
 	lcall serial_write_crlf
 	sjmp diag_loop
@@ -64,8 +68,81 @@ diag_spi_send:
 
 diag_sd_test:
 	lcall disk_init
+	lcall serial_write_byte
+	lcall serial_write_crlf
+
+	lcall disk_cmd8
+	lcall spi_poll_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+
 	ljmp diag_cleanup
 
+diag_sd_msg:
+	lcall serial_read_ascii_byte
+	push acc
+	lcall serial_write_byte
+	lcall serial_write_crlf
+
+	;; Load 4 bytes of args.
+	lcall serial_read_ascii_byte
+	push acc
+	lcall serial_write_byte
+	lcall serial_read_ascii_byte
+	push acc
+	lcall serial_write_byte
+	lcall serial_read_ascii_byte
+	push acc
+	lcall serial_write_byte
+	lcall serial_read_ascii_byte
+	push acc
+	lcall serial_write_byte
+	lcall serial_read_ascii_byte
+	push acc
+	lcall serial_write_byte
+
+	lcall serial_write_crlf
+
+	;; Store args into registers
+	pop acc
+	mov r4, a
+	pop acc
+	mov r3, a
+	pop acc
+	mov r2, a
+	pop acc
+	mov r1, a
+	pop acc
+	mov r0, a
+	pop acc
+
+	;; Send command.
+	lcall disk_send_command
+
+	;; Get response.
+	lcall spi_poll_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+	lcall spi_read_byte
+	lcall serial_write_byte
+
+	ljmp diag_cleanup
 diag_get_address:
 ;;; Read a 16-bit address over serial into dptr.
 ;;; Echo the received bytes.
